b86111294a88a52f574403e9a3e7cf3c2418e500
Ada Barbulescu <adabarbu2000@yahoo.com>
2023-11-19 16:12:46 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+    const [childClicked, setChildClicked] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+
+    //GPS person geolocation
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
+
+    useEffect(() => {
+        if (bounds.sw && bounds.ne) {
+            setIsLoading(true);
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setFilteredPlaces([]);
+                    setIsLoading(false);
+                })
+        }
+    }, [type, bounds]);
+
+
     return (
         <>
             <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{ width: '100%' }}>
-                <Grid item xs={12} md={4}>
-                    <List />
+            <Header setCoordinates={setCoordinates} />
+            <Grid container style={{ height: '100vh', width: '100%', margin: 0 }}>
+                <Grid item xs={12} sm={6} md={4} lg={3} style={{ overflowY: 'auto' }}>
+                    <List
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
                 </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map />
+                <Grid item xs={12} sm={6} md={8} lg={9} style={{ position: 'relative' }}>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                    />
                 </Grid>
 
             </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, useMediaQuery, Typography } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from "@material-ui/lab/Rating";
 
-const Map = () => {
+import useStyles from './styles';
+import mapStyles from '../../mapStyles';
+
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
+    const isDesktop = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+                }}
+                onChildClick={(child) => setChildClicked(child)}
+            >
+                {places?.map((place, i) => (
+                    <div className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                    >
+                        {
+                            !isDesktop ? (
+                                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg"}
+                                        alt={place.name} />
+                                    <Rating size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )}
+                    </div>
+                ))
+                }
+            </GoogleMapReact >
+        </div >
     )
 }
 
 export default Map;
+
